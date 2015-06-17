7bfc98aee7f88e81a9dd873738d28f8a3db46de9
Jason Tsukamoto <jasontsukamoto@gmail.com>
2015-06-16 15:08:03 -1000

    adds pixel painter logo

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,8 +1,8 @@
 window.onload = function() {
   var pxlPainter = GridMaker();
   pxlPainter.createGrid();
-  var colorPalette = new pxlPainter.createGrid(6,6, 'colorPalette');
-  var mainGrid = new pxlPainter.createGrid(15, 15, 'mainGrid');
+  var colorPalette = new pxlPainter.createGrid(6,6, 'colorPalette', 'color');
+  var mainGrid = new pxlPainter.createGrid(15, 15, 'mainGrid', 'grid');
   var CSS_COLOR_NAMES = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
   pxlPainter.pxlColor(CSS_COLOR_NAMES);
   pxlPainter.selectColor();
@@ -16,9 +16,10 @@ var GridMaker = function() {
   var erase;
 
   //Class to create grids
-  var createGrid = function(x, y, id) {
+  var createGrid = function(x, y, id, type) {
     //sets divEl equal to pixelPainter div
-    var divEl = document.getElementById('pixelPainter');
+    var divEl = document.getElementById(type + 'Container');
+    console.log(divEl);
 
     var container = document.createElement('div');
     container.id = id;
@@ -66,23 +67,24 @@ var GridMaker = function() {
     });
   };
 
-  var eraseButton = function(paint) {
-    var divEl = document.getElementById('pixelPainter')
+  var eraseButton = function() {
+    var cPalette = document.getElementById('colorPalette')
     var button = document.createElement('button');
+    button.setAttribute('id', 'erase');
     button.innerHTML = 'erase';
-    divEl.appendChild(button);
+    cPalette.appendChild(button);
     button.addEventListener('click', function(event) {
       selected = 'transparent';
     });
   };
 
   var clearButton = function() {
-    var divEl = document.getElementById('pixelPainter')
+    var cPalette = document.getElementById('colorPalette')
     var button = document.createElement('button');
+    button.setAttribute('id', 'clear');
     button.innerHTML = 'clear'
-    divEl.appendChild(button);
+    cPalette.appendChild(button);
     var canvas = document.getElementsByClassName('mainGrid_cells');
-    console.log('canvas',canvas);
     button.addEventListener('click', function(event) {
       for (var i = 0; i < canvas.length; i++) {
         canvas[i].style.background = 'transparent';
